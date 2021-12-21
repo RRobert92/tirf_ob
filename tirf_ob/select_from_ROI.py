8bd344b19490a601dd733e05e18bd8ca15cdb1f1
Robert Kiewisz <56911280+RRobert92@users.noreply.github.com>
2021-12-21 15:12:38 +0100

    loop over whole folder

--- tirf_ob/select_from_ROI.py
+++ tirf_ob/select_from_ROI.py
@@ -1,6 +1,9 @@
 import os
+from os.path import join
+from typing import Optional
 
 import click
+import tifffile
 
 from tirf_ob import version
 from tirf_ob.utils.interpolate import interpolation_3D
@@ -8,13 +11,12 @@ from tirf_ob.utils.load_data import *
 
 
 @click.command()
-@click.option('-dir_img', '--dir_img',
+@click.option('-dir', '--dir',
+              default=os.getcwd(),
               help='Directory to the image in *.tif format.',
               show_default=True)
-@click.option('-dir_csv', '--dir_csv',
-              help='Directory to the coordinates in *.csv format.',
-              show_default=True)
 @click.option('-o', '--output',
+              default=join(os.getcwd(), 'output'),
               help='Output directory for saving processed data.',
               show_default=True)
 @click.option('-m', '--mask_size',
@@ -22,55 +24,73 @@ from tirf_ob.utils.load_data import *
               help='Size of the cropping mask',
               show_default=True)
 @click.version_option(version=version)
-def main(dir_img: str,
-         dir_csv: str,
-         output: str,
-         mask_size: int):
+def main(dir: str,
+         mask_size: int,
+         output: Optional[str] = None):
     """ Load data """
-    image = load_image_to_numpy(dir_img)
-    image_size = image.shape
+    img_list = os.listdir()
+    if output is None:
+        output = join(dir, 'output')
 
-    coord = load_csv_to_numpy(dir_csv)
+    for img_dir in img_list:
 
-    """ Interpolate coordinate"""
-    coord = interpolation_3D(coord)
-    idx = np.ravel_multi_index(coord.reshape(coord.shape[0], - 1).T,
-                               coord.max(0).ravel() + 1)
-    coord = coord[np.sort(np.unique(idx, return_index=True)[1])]
-
-    """ Main loop to corp image """
-    corpped_image = np.zeros((image_size[0], mask_size, mask_size))
-    for i in range(image_size[0]):
-        idx = np.where(coord[:, 2] == (i + 1))[0]
-
-        """ Find row with current Z position """
-        if len(idx) == 1:
-            idx = idx
-        elif len(idx) == 0:
-            assert i != 0 or i != (image_size[0] - 1), 'For the interpolation first and the last point are needed!'
-            past = np.where(coord[:, 2] == i)[0]
-            feature = np.where(coord[:, 2] == (i + 2))[0]
-            idx = (coord[past, :] + coord[feature, :]) / 2
+        if img_dir[-3:] == 'tif':
+            csv_dir = img_dir[:-3] + 'csv'
+            name = img_dir[:-4]
+        elif img_dir[-4:] == 'tiff':
+            csv_dir = img_dir[:-4] + 'csv'
+            name = img_dir[:-5]
         else:
-            idx = np.array(int(round(idx.mean(), 0)))
+            csv_dir = None
 
-        """ Define cropping area """
-        if idx.size == 1:
-            crop_y_start, crop_y_stop = coord[idx, 0] - mask_size / 2, coord[idx, 0] + mask_size / 2
-            crop_x_start, crop_x_stop = coord[idx, 1] - mask_size / 2, coord[idx, 1] + mask_size / 2
-        if idx.size == 3:
-            crop_y_start, crop_y_stop = idx[0, 0] - mask_size / 2, idx[0, 0] + mask_size / 2
-            crop_x_start, crop_x_stop = idx[0, 1] - mask_size / 2, idx[0, 1] + mask_size / 2
+        if csv_dir is not None:
+            image = load_image_to_numpy(join(dir, img_dir))
+            image_size = image.shape
 
-        """ Crop image and save """
-        corpped_image[i, :, :] = image[
-                                 i,
-                                 int(crop_x_start):int(crop_x_stop),
-                                 int(crop_y_start): int(crop_y_stop)
-                                 ]
+            coord = load_csv_to_numpy(csv_dir)
 
-    tifffile.imsave(os.join(output + 'output.tif'),
-                    corpped_image)
+            """ Interpolate coordinate"""
+            coord = interpolation_3D(coord)
+            idx = np.ravel_multi_index(coord.reshape(coord.shape[0], - 1).T,
+                                       coord.max(0).ravel() + 1)
+            coord = coord[np.sort(np.unique(idx, return_index=True)[1])]
+
+            """ Main loop to corp image """
+            corpped_image = np.zeros((image_size[0], mask_size, mask_size))
+            for i in range(image_size[0]):
+                idx = np.where(coord[:, 2] == (i + 1))[0]
+
+                """ Find row with current Z position """
+                if len(idx) == 1:
+                    idx = idx
+                elif len(idx) == 0:
+                    assert i != 0 or i != (
+                        image_size[0] - 1), 'For the interpolation first and the last point are needed!'
+                    past = np.where(coord[:, 2] == i)[0]
+                    feature = np.where(coord[:, 2] == (i + 2))[0]
+                    idx = (coord[past, :] + coord[feature, :]) / 2
+                else:
+                    idx = np.array(int(round(idx.mean(), 0)))
+
+                """ Define cropping area """
+                if idx.size == 1:
+                    crop_y_start, crop_y_stop = coord[idx, 0] - \
+                        mask_size / 2, coord[idx, 0] + mask_size / 2
+                    crop_x_start, crop_x_stop = coord[idx, 1] - \
+                        mask_size / 2, coord[idx, 1] + mask_size / 2
+                if idx.size == 3:
+                    crop_y_start, crop_y_stop = idx[0, 0] - \
+                        mask_size / 2, idx[0, 0] + mask_size / 2
+                    crop_x_start, crop_x_stop = idx[0, 1] - \
+                        mask_size / 2, idx[0, 1] + mask_size / 2
+
+                """ Crop image and save """
+                corpped_image[i, :, :] = image[i,
+                                               int(crop_x_start):int(crop_x_stop),
+                                               int(crop_y_start): int(crop_y_stop)]
+
+                tifffile.imsave(join(output, name + '_trim.tif'),
+                                corpped_image)
 
 
 if __name__ == '__main__':
